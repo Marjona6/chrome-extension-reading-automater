3e34801b348af5ce118ff2366723252e72e85424
Meryem <meryem@spechy.com>
2018-01-19 10:33:25 +0300

    Get URLs on right click

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-function addArticle(link) {
-	alert('adding article: ', link);
-	// get link
-	// save link somewhere... where?
+function addArticle() {
+    return function (info, tab) {
+        var url = info.linkUrl;
+        alert(url);
+        // below: need logic to first check sync storage for the key
+        // if it exists, get its value and add this url on to the end of the list of urls
+        // if it doesn't exist, set it and add this url as the value
+        // anytime a user adds a new value, it should be added on to the end (separated by commas? spaces?)
+        //        chrome.storage.sync.set({'key': 'value'});
+        //        chrome.tabs.create({
+        //            url: url,
+        //        });
+    }
+    // save link somewhere... where?
 }
 
+// for viewing a list of saved articles
+
+// for loading one saved article to read
+
+// for finishing a saved article and loading the next
+
+// for automatically loading the reading browser at a set time/date
+
+// for setting the time and date and reading browser options
+
 chrome.contextMenus.create({
-	title: 'Add link to Reading List',
-	contexts: ["link"],
-	onclick: addArticle
+    title: 'Add link to Reading List',
+    contexts: ["link"],
+    onclick: addArticle()
 });
