31774f34ed9235b8ae3ba0e5c8d7fd77462315fd
Meryem Yanar <meryem.yanaryanar@gmail.com>
2019-04-03 22:13:07 +0200

    Add options/settings skeleton

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-function addArticle() {
-    return function (info, tab) {
-        var url = info.linkUrl;
-        alert(url);
-        // below: need logic to first check sync storage for the key
-        // if it exists, get its value and add this url on to the end of the list of urls
-        // if it doesn't exist, set it and add this url as the value
-        // anytime a user adds a new value, it should be added on to the end (separated by commas? spaces?)
-        //        chrome.storage.sync.set({'key': 'value'});
-        //        chrome.tabs.create({
-        //            url: url,
-        //        });
-    }
-    // save link somewhere... where?
-}
+const addArticle = () => {
+  return (info, tab) => {
+    var url = info.linkUrl;
+    alert(url);
+    // below: need logic to first check sync storage for the key
+    // if it exists, get its value and add this url on to the end of the list of urls
+    // if it doesn't exist, set it and add this url as the value
+    // anytime a user adds a new value, it should be added on to the end (separated by commas? spaces?)
+    //        chrome.storage.sync.set({'key': 'value'});
+    //        chrome.tabs.create({
+    //            url: url,
+    //        });
+  };
+  // save link somewhere... where?
+};
 
 // for viewing a list of saved articles
 
@@ -20,12 +20,29 @@ function addArticle() {
 
 // for finishing a saved article and loading the next
 
+// everything below is copied and pasted into popup.js for now; will DRY things up later
+
 // for automatically loading the reading browser at a set time/date
+const openBrowserWindow = () => {
+  chrome.windows.create({
+    url: "localhost:6800",
+    state: "fullscreen",
+    focused: true,
+    type: "normal"
+  });
+};
+
+const openBrowserWindowAtFixedTime = fixedTime => {
+  const now = new Date();
+  const fixedDateInMilliseconds = new Date(fixedTime);
+  const intervalInMilliseconds = fixedDateInMilliseconds - now;
+  setTimeout(openBrowserWindow, intervalInMilliseconds);
+};
 
 // for setting the time and date and reading browser options
 
-chrome.contextMenus.create({
-    title: 'Add link to Reading List',
-    contexts: ["link"],
-    onclick: addArticle()
-});
+// chrome.contextMenus.create({
+//   title: "Add link to Reading List",
+//   contexts: ["link"],
+//   onclick: addArticle()
+// });
--- popup.js
+++ popup.js
@@ -10,3 +10,53 @@
 // users should still be able to add to their reading list while in reading mode. use example: let's say i'm reading an article from my list and it links to another article i want to read. instead of opening it up in a separate tab to read right now or "later" (never going to happen), i can right click and add it to the list, which will append it to the end of the current reading list i'm reading right now.
 // to set a specific reading time and open the reading list window at that time: https://developer.chrome.com/extensions/alarms !!!
 // I may need to use some sort of DB (MongoDB? mLab?) to save URLs rather than chrome.storage sync... maybe alarms and settings can stay in chrome.storage? not sure how large they will be
+
+// for automatically loading the reading browser at a set time/date
+const openBrowserWindow = url => {
+  alert("boy howdy!");
+  chrome.windows.create({
+    url: url,
+    state: "fullscreen",
+    focused: true,
+    type: "normal"
+  });
+};
+// always waits the document to be loaded when shown
+window.addEventListener("DOMContentLoaded", () => {
+  // opens a communication between scripts
+  //   const port = chrome.runtime.connect();
+
+  // listens to the click of the button into the popup content
+  document.getElementById("settings").addEventListener("click", () => {
+    alert("wahoo");
+    openBrowserWindow("options.html");
+    // sends a message throw the communication port
+    // port.postMessage({
+    //     'from': 'popup',
+    //     'start': 'Y'
+    // });
+  });
+
+  const datepicker = document.getElementById("datepicker");
+  datepicker.addEventListener("click", () => {
+    M.Datepicker.init(datepicker, options); // filler, does nothing right now
+  });
+
+  const startReadingNow = document.getElementById("start-reading-now");
+  startReadingNow.addEventListener("click", openBrowserWindow);
+
+  const openBrowserWindowAtFixedTime = fixedTime => {
+    const now = new Date();
+    const fixedDateInMilliseconds = new Date(fixedTime);
+    const intervalInMilliseconds = fixedDateInMilliseconds - now;
+    setTimeout(openBrowserWindow, intervalInMilliseconds);
+  };
+
+  // for setting the time and date and reading browser options
+
+  //   chrome.contextMenus.create({
+  //     title: "Add link to Reading List",
+  //     contexts: ["link"],
+  //     onclick: addArticle()
+  //   });
+});
